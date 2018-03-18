0c944595b483b5d40161b534859426a00c644f25
Keone Martin <keonemartin@gmail.com>
2018-03-18 19:33:36 +1100

    added test cases and changed minimum length of card number to 14 to accept diner's card numbers

--- controllers/validator.js
+++ controllers/validator.js
@@ -1,6 +1,36 @@
 const validate = (number) => {
-  console.log(number);
-  return false;
+  if (!parseInt(number)) return false; // user didn't enter a number
+  // NOTE card length assumptions below are based off a quick googling, needs more research
+  if (number.length < 14) return false; // diners club cards have 14 digits
+  if (number.length > 19) return false; // 19 digits is the maximum length for a credit card number
+
+  // Luhn formula see https://en.wikipedia.org/wiki/Luhn_algorithm for more details
+  // split the string into an array for processing
+  let numbers = number.split('');
+
+  // iterate from right, double every second number
+  let toDouble = false; // should this number be doubled
+  let total = 0; // holds the sum of all numbers
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    let current = parseInt(numbers[i]); // get the current number
+    if (toDouble){
+      let addToTotal = 0; // instantiate an output variable
+      current *= 2; // double the number
+      // Loop to add the two digits together if greater than 9
+      while(current) {
+        addToTotal += current % 10;
+        current = Math.floor(current / 10);
+      }
+      total += addToTotal;
+    } else {
+      total += current;
+    }
+    // toggle between each number
+    toDouble = !toDouble;
+  }
+
+// to qualify for the luhn formula the total modulo 10 must equal to 0
+  return total % 10 === 0;
 }
 
 module.exports = validate;
--- test/test.js
+++ test/test.js
@@ -1,1 +1,84 @@
 const assert = require('assert');
+const validator = require('../controllers/validator');
+
+describe('validator', function() {
+  it('should return true for valid card numbers', function() {
+      assert.ok(validator(testNumbersTrue[0]));
+      assert.ok(validator(testNumbersTrue[1]));
+      assert.ok(validator(testNumbersTrue[2]));
+      assert.ok(validator(testNumbersTrue[3]));
+      assert.ok(validator(testNumbersTrue[4]));
+      assert.ok(validator(testNumbersTrue[5]));
+      assert.ok(validator(testNumbersTrue[6]));
+      assert.ok(validator(testNumbersTrue[7]));
+      assert.ok(validator(testNumbersTrue[8]));
+      assert.ok(validator(testNumbersTrue[9]));
+      assert.ok(validator(testNumbersTrue[10]));
+  })
+
+  it('should return false for invalid card numbers', function() {
+      assert.ok(!validator(testNumbersFalse[0]));
+      assert.ok(!validator(testNumbersFalse[1]));
+      assert.ok(!validator(testNumbersFalse[2]));
+      assert.ok(!validator(testNumbersFalse[3]));
+      assert.ok(!validator(testNumbersFalse[4]));
+      assert.ok(!validator(testNumbersFalse[5]));
+      assert.ok(!validator(testNumbersFalse[6]));
+      assert.ok(!validator(testNumbersFalse[7]));
+      assert.ok(!validator(testNumbersFalse[8]));
+      assert.ok(!validator(testNumbersFalse[9]));
+      assert.ok(!validator(testNumbersFalse[10]));
+  })
+});
+
+// fake credit card numbers for testing from freeformatter.com
+const testNumbersTrue = [
+// VISA:
+'4716836821877960',
+// MasterCard:
+'2221003346919023',
+// American Express (AMEX):
+'374856518864361',
+// Discover:
+'6011470981698573',
+// JCB:
+'3543049996586159327',
+// Diners Club - North America:
+'5487871917053089',
+// Diners Club - Carte Blanche:
+'30593638481075',
+// Diners Club - International:
+'36289675638255',
+// Maestro:
+'5018345002894549',
+// Visa Electron:
+'4175009551779160',
+// InstaPayment:
+'6382713486416954'
+]
+
+// The same numbers incremented by 1 to make them inaccurate
+const testNumbersFalse = [
+// VISA:
+'4716836821877961',
+// MasterCard:
+'2221003346919024',
+// American Express (AMEX):
+'374856518864362',
+// Discover:
+'6011470981698574',
+// JCB:
+'3543049996586159328',
+// Diners Club - North America:
+'5487871917053080',
+// Diners Club - Carte Blanche:
+'30593638481076',
+// Diners Club - International:
+'36289675638256',
+// Maestro:
+'5018345002894540',
+// Visa Electron:
+'4175009551779161',
+// InstaPayment:
+'6382713486416955'
+]
